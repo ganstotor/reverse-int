5d37fa4ec1c0d040b249f1abfda761dcd2a12615
ganstotor <ganstotor@gmail.com>
2024-09-07 22:51:51 +0300

    init: created reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+
+        let reversedString = n.toString().split('').reverse().join('');
+
+        let reversedn = parseInt(reversedString, 10);
+
+        return reversedn;
 }
